8e2f5cdaae25f538fb5939bb861c3d742e7ea797
mpcosta <markkcosta@gmail.com>
2024-03-16 18:44:12 +0000

    Updating frontend related application for prometheus + jaeger

--- reference-app/frontend/app.py
+++ reference-app/frontend/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
+from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
 
 app = Flask(__name__)
+metrics = GunicornInternalPrometheusMetrics(app)
 
+def child_exit(server, worker):
+    GunicornInternalPrometheusMetrics.mark_process_dead_on_child_exit(worker.pid)
 
 @app.route("/")
 def homepage():
